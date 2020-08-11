33cc84fa797c3413c93cd9c6957f3100e946b6fa
Madeleine Bizard <bizard.madeleine@gmail.com>
2020-08-11 16:18:40 +0200

    exo 3 sans css car j'y suis depuis 1h et que c'est interminaaaaaable

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+require 'faker'
+Article.destroy_all
+
+10.times do
+  Article.create(
+      title:   Faker::GreekPhilosophers.name,
+      content: Faker::GreekPhilosophers.quote
+    )
+end
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
